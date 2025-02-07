a578dc8258662883d884e2a497d4caf5c3e589f7
Lek <ligioner29@mail.ru>
2025-02-08 06:43:13 +0800

    проверка изменений.не верно выводятся популярные посты

--- blog/views.py
+++ blog/views.py
@@ -31,15 +31,33 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
-    all_posts = Post.objects.select_related('author')\
-            .prefetch_related('tags')\
-            .annotate(like_count=Count('likes'))
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
+
+def index(request):
+    all_posts = (
+        Post.objects
+        .select_related('author')
+        .prefetch_related('tags')
+        .annotate(
+            like_count=Count('likes'),
+            comments_count=Count('comments'))
+    )
     sorted_posts = all_posts.order_by('-like_count')
     most_popular_posts = sorted_posts[:5]
 
-    fresh_posts = Post.objects.order_by('published_at')
+    fresh_posts = Post.objects.order_by('-published_at')
     most_fresh_posts = list(fresh_posts)[:5]
 
     all_tags = Tag.objects.annotate(posts_count=Count('posts'))
@@ -48,7 +66,7 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
         'page_posts': [serialize_post(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
@@ -87,13 +105,17 @@ def post_detail(request, slug):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = (
+        Post.objects
+        .annotate(like_count=Count('likes'), comments_count=Count('comments'))
+        .order_by('-like_count')[:5]
+    )  # TODO. Как это посчитать?
 
     context = {
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
