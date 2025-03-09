0fc70753aa63eea02dea9cdb12f99e58a9aa5a7a
Lek <ligioner29@mail.ru>
2025-03-10 06:42:20 +0800

    formatted the models

--- blog/models.py
+++ blog/models.py
@@ -38,6 +38,7 @@ class PostQuerySet(models.QuerySet):
     def fresh(self):
         return self.order_by('-published_at')
 
+
 class TagQuerySet(models.QuerySet):
     def popular(self):
         return self.annotate(posts_count=Count('posts')).order_by('-posts_count')[:5]
@@ -58,8 +59,6 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации', db_index=True)
-    objects = PostQuerySet.as_manager()
-
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -75,35 +74,38 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Теги')
 
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+    objects = PostQuerySet.as_manager()
 
     class Meta:
         ordering = ['-published_at']
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
 
-
-class Tag(models.Model):
-    title = models.CharField('Тег', max_length=20, unique=True)
-    objects = TagQuerySet.as_manager()
-
     def __str__(self):
         return self.title
 
-    def clean(self):
-        self.title = self.title.lower()
-
     def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
+        return reverse('post_detail', args={'slug': self.slug})
+
+
+class Tag(models.Model):
+    title = models.CharField('Тег', max_length=20, unique=True)
+
+    objects = TagQuerySet.as_manager()
 
     class Meta:
         ordering = ['title']
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
 
 
 class Comment(models.Model):
@@ -120,12 +122,14 @@ class Comment(models.Model):
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
 
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
+
+
 
 
