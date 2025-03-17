36512d29f40920131046c92c85720c3f06f250c9
Lek <ligioner29@mail.ru>
2025-03-18 06:40:28 +0800

    deleted django toolbar

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'blog',
 ]
 
@@ -32,7 +31,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
